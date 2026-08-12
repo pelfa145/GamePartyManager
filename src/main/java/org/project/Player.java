40973ee218197dbcd6107be7d8a4385e91d4b81a
pelfa145 <cymig213@gmail.com>
2026-08-13 00:29:51 +0800

    cleaned code

--- src/main/java/org/project/Player.java
+++ src/main/java/org/project/Player.java
@@ -37,7 +37,20 @@ public class Player {
     int getLevel(){return level;}
     String getUsername(){return username;}
     //setters
-    void setUsername(String newUser){
+    void takeDamage(int amount){
+        this.health -= amount;
+        System.out.println(this.username+" took "+amount+" Damage! Remaining HP: "+this.health);
+    }
+    void heal(int amount){
+        this.health += amount;
+        System.out.println(this.username+" has healed +"+amount+"HP.");
+    }
+    void levelUp(){
+        this.level ++;
+        System.out.println(this.username+" has leveled up!");
+    }
+    void displayStats(){
+        System.out.println(this.username+"\nHP: "+this.health+"\nLevel: "+this.level);
     }
 
 
